0560db44e41c9874d92461ff600ee7ad42918711
Simranjeet Singh <kahlonsimran93@gmail.com>
2020-01-01 12:15:47 -0500

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Simranjeet  Singh on 2020-01-01.
+//  Copyright © 2020 Simranjeet  Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
